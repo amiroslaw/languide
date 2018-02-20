e74bc583e6878dd549b4862ccb64f1a4a1cdd4c5
Arkadiusz <arek47@gmail.com>
2018-02-21 00:52:09 +0100

    add config files, change bootstrap version

--- src/main/java/xyz/miroslaw/languide/controller/ArticleController.java
+++ src/main/java/xyz/miroslaw/languide/controller/ArticleController.java
@@ -1,24 +1,47 @@
 package xyz.miroslaw.languide.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import xyz.miroslaw.languide.command.ArticleCommand;
 import xyz.miroslaw.languide.exception.NotFoundException;
+import xyz.miroslaw.languide.model.Article;
+import xyz.miroslaw.languide.service.ArticleService;
+
+import javax.validation.Valid;
 
 @Slf4j
+@Controller
 public class ArticleController {
-    @GetMapping("/articles")
-    public String showArticles() {
+    private ArticleService articleService;
 
-        return "article";
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+
+    @PostMapping("/user/{userId}/article")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String pair(@Valid @ModelAttribute("articleCommand") ArticleCommand articleCommand, Model model, BindingResult bindingResult, @PathVariable int userId) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(objectError -> log.debug(objectError.toString()));
+            return "index";
+        }
+        //todo add parameter and direct to view or articleform
+        Article article = articleService.createArticle(articleCommand);
+        model.addAttribute("article", article);
+        return "/article/view";
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception){
+    public ModelAndView handleNotFound(Exception exception) {
 
         log.error("Handling not found exception");
         log.error(exception.getMessage());
@@ -29,5 +52,11 @@ public class ArticleController {
         modelAndView.addObject("exception", exception);
 
         return modelAndView;
+    }
+
+    @GetMapping("/articles")
+    public String showArticles() {
+
+        return "article";
     }
 }
--- src/main/java/xyz/miroslaw/languide/controller/HomeController.java
+++ src/main/java/xyz/miroslaw/languide/controller/HomeController.java
@@ -1,51 +1,25 @@
 package xyz.miroslaw.languide.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import xyz.miroslaw.languide.command.ArticleCommand;
-import xyz.miroslaw.languide.model.Article;
-import xyz.miroslaw.languide.service.ArticleService;
-
-import javax.validation.Valid;
 
 @Slf4j
 @Controller
 public class HomeController {
 
-    private ArticleService articleService;
 
-    @Autowired
-    public HomeController(ArticleService articleService) {
-        this.articleService = articleService;
-    }
 
     @GetMapping({"/", ""})
     public String getHomePage(Model model) {
         ArticleCommand articleCommand = new ArticleCommand();
         model.addAttribute("articleCommand", articleCommand);
-        log.debug("index page");
         return "index";
     }
-
-    @PostMapping({"/", ""})
-    @ResponseStatus(HttpStatus.CREATED)
-    public String pair(@Valid @ModelAttribute("articleCommand") ArticleCommand articleCommand, Model model, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            bindingResult.getAllErrors().forEach(objectError -> log.debug(objectError.toString()));
-            return "index";
-        }
-        Article article = articleService.createArticle(articleCommand);
-        model.addAttribute("article", article);
-        return "/article/view";
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        return "/user/login";
     }
-
-
 }
